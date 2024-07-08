d8ec29d919df5b9f6d915183b89ab35d5907e22e
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-08 14:55:39 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -5,21 +5,54 @@ function getComputerChoice () {
     let computerChoice;
     switch (rand) {
         case 0:
-            computerChoice = 'Rock';
+            computerChoice = 'rock';
             break;
         case 1:
-            computerChoice = 'Paper';
+            computerChoice = 'paper';
             break;
         case 2:
-            computerChoice = 'Scissors';
+            computerChoice = 'scissors';
             break;
     }
-    console.log(computerChoice);
     return computerChoice;
 }
 
 function getHumanChoice () {
     let humanChoice = prompt('Enter your choice (rock, paper, or scissors)');
-    console.log(humanChoice);
     return humanChoice;
 }
+
+let computerScore = 0;
+let humanScore = 0;
+let tieScore = 0;
+
+function playRound (computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log('tie');
+        tieScore++;
+    } else if (computerChoice === 'rock' && humanChoice === 'paper') {
+        console.log('You win! Paper beats rock.');
+        humanScore++;
+    } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+        console.log('You lose! Rock beats scissors.');
+        computerScore++;
+    } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+        console.log('You lose! Paper beats rock.');
+        computerScore++;
+    } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
+        console.log('You win! Scissors beat paper.');
+        humanScore++;
+    } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
+        console.log('You win! Rock beats scissors.');
+        humanScore++;
+    } else if (computerChoice === 'scissors' && humanScore === 'paper') {
+        console.log('You lose! Scissors beat paper.');
+        computerScore++;
+    }
+}
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+humanChoice = humanChoice.toLowerCase();
+
+playRound(computerChoice, humanChoice);
